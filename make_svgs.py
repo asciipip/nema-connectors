5a82f0f6562208d1fb23cf20be9fb4b11f919d3f
Phil! Gold <phil_g@pobox.com>
2018-05-23 10:34:05 -0400

    Abstract drawing routines into a base class.

--- make_svgs.py
+++ make_svgs.py
@@ -27,8 +27,49 @@ class SquareConductor:
                             (self.width, self.height),
                             fill='black')
 
-class NEMA_1_15:
+
+class NEMABase:
     def __init__(self):
+        self.receptacle_diameter = None
+        self.plug_diameter = None
+        
+    def draw(self, diameter, conductors):
+        drawing_width = diameter + OUTLINE_WIDTH * 2
+        drawing = svgwrite.Drawing(size=(round(drawing_width * SCALE + 0.5),
+                                         round(drawing_width * SCALE + 0.5)),
+                                   viewBox='0 0 {0} {0}'.format(drawing_width))
+        g = drawing.g(transform='translate({0} {0})'.format(drawing_width / 2))
+        drawing.add(g)
+
+        g.add(drawing.circle(r=diameter/2, fill='white', stroke='black',
+                             stroke_width=OUTLINE_WIDTH))
+        for conductor in conductors.values():
+            g.add(conductor.draw(drawing))
+        
+        return drawing
+        
+    def draw_receptacle(self):
+        if self.receptacle_diameter is None:
+            return None
+        else:
+            return self.draw(self.receptacle_diameter, {k: v[0] for k, v in self.conductors.items()})
+
+    def draw_plug(self):
+        if self.plug_diameter is None:
+            return None
+        else:
+            return self.draw(self.plug_diameter, {k: v[1] for k, v in self.conductors.items()})
+
+    def save(self):
+        if self.receptacle_diameter is not None:
+            self.draw_receptacle().saveas('NEMA_{}R.svg'.format(self.name))
+        if self.plug_diameter is not None:
+            self.draw_plug().saveas('NEMA_{}P.svg'.format(self.name))
+
+class NEMA_1_15(NEMABase):
+    def __init__(self):
+        super().__init__()
+        
         self.name = '1-15'
 
         self.receptacle_diameter = 1.531
@@ -55,31 +96,6 @@ class NEMA_1_15:
             ),
         }
 
-    def draw(self, diameter, conductors):
-        drawing_width = diameter + OUTLINE_WIDTH * 2
-        drawing = svgwrite.Drawing(size=(round(drawing_width * SCALE + 0.5),
-                                         round(drawing_width * SCALE + 0.5)),
-                                   viewBox='0 0 {0} {0}'.format(drawing_width))
-        g = drawing.g(transform='translate({0} {0})'.format(drawing_width / 2))
-        drawing.add(g)
-
-        g.add(drawing.circle(r=diameter/2, fill='white', stroke='black',
-                             stroke_width=OUTLINE_WIDTH))
-        for conductor in conductors.values():
-            g.add(conductor.draw(drawing))
-        
-        return drawing
-        
-    def draw_receptacle(self):
-        return self.draw(self.receptacle_diameter, {k: v[0] for k, v in self.conductors.items()})
-
-    def draw_plug(self):
-        return self.draw(self.plug_diameter, {k: v[1] for k, v in self.conductors.items()})
-
-    def save(self):
-        self.draw_receptacle().saveas('NEMA_{}R.svg'.format(self.name))
-        self.draw_plug().saveas('NEMA_{}P.svg'.format(self.name))
-        
 if __name__ == '__main__':
     NEMA_1_15().save()
     
